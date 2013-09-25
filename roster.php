499bb3a12aecae677bbbe204c8e3395d05bdfdbf
Ron Pringle <pringler@bouldercolorado.gov>
2013-09-25 16:11:15 -0400

    fixed missing bracket and escaped double quotes

--- roster.php
+++ roster.php
@@ -14,13 +14,14 @@
 			echo "<h2>Instructions</h2>";
 			echo "<p>Please add your name to the attendees array in the code and increment the version number by one minor increment (i.e. version 1.1, 1.2, etc.). Commit your code, push the commit to your cloned repository and then issue a pull request to the master repository.</p>\n";
 			echo "<p>Don't worry about messing anything up, that's why we're using Git in the first place!</p>\n";
-			echo "<p>Need help? Contact me at <a href="mailto:pringler@bouldercolorado.gov">pringler@bouldercolorado.gov</a>.</p>\n";
+			echo "<p>Need help? Contact me at <a href=\"mailto:pringler@bouldercolorado.gov\">pringler@bouldercolorado.gov</a>.</p>\n";
 			echo "<h2>Session attendee list</h2>\n";
 			if (isset($attendees))
 			{
 				echo "<ul>\n";
 				
 				foreach($attendees as $attendee)
+				{
 					echo '<li>'.$attendee.'</li>'."\n";
 				}
 				
